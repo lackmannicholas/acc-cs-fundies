d00e3201ce2617586a990e09bd464bff5432b8bb
Nick Lackman <lackmannicholas@gmail.com>
2020-12-29 13:35:54 -0800

    finishing finishing assignment 2

--- cs-fundies-1/assignment-1/main.cpp
+++ cs-fundies-1/assignment-1/main.cpp
@@ -8,10 +8,16 @@
 **/
 
 // You need to include some header(s) here!
-
+#include <iostream>
 
 // You need to define your main() function here!
+int main() {
 
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Greetings from Illinois!" << std::endl;
+
+    return 0;
+}
 
 // Notes:
 
